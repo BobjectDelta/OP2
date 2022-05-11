99dd2614cc186a4d791e89d63c64736eae42bbb6
BobjectDelta <90173834+BobjectDelta@users.noreply.github.com>
2022-05-11 23:37:43 +0300

    Add files via upload

---
+++ Lab_2/Lab2/Lab2/FileEditing.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2;
+
+namespace Lab2
+{
+    internal class FileEditing
+    {
+        public static List<Item> EditItemList(List<Item> items)
+        {
+            double prodExpireInterval = 0;
+            double todayExpireInterval = 0;
+            DateTime prodDate = new DateTime();
+            DateTime expireDate = new DateTime();
+            List<Item> editedItems = new List<Item>();
+            Item currentItem = new Item("", "", "", 0);
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                currentItem = items[i];               
+                prodDate = Convert.ToDateTime(currentItem.GetProdDate());
+                expireDate = Convert.ToDateTime(currentItem.GetExpireDate());
+                prodExpireInterval = (expireDate.Subtract(prodDate).TotalDays);
+                todayExpireInterval = (expireDate.Subtract(DateTime.Today).TotalDays);
+
+                if(todayExpireInterval < prodExpireInterval/10)
+                {
+                    editedItems.Add(currentItem);
+                }
+            }
+
+            return editedItems;
+        }
+
+        public static List<Item> GetLastDaysItems(List<Item> items)
+        {
+            List<Item> lastDaysItems = new List<Item>();
+            Item currentItem = new Item("", "", "", 0);
+            DateTime prodDate = new DateTime();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                currentItem = items[i];
+                prodDate = Convert.ToDateTime(currentItem.GetProdDate());
+                if(((DateTime.Today).Subtract(prodDate)).TotalDays <= 10)
+                {
+                    lastDaysItems.Add(currentItem);
+                }
+            }
+
+            return lastDaysItems;
+        }
+
+        public static void PrintList(List<Item> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i].GetAllAtributes());
+            }
+        }
+    }
+}
---
+++ Lab_2/Lab2/Lab2/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Item
+    {
+        private string _name;
+        private string _productionDate;
+        private string _expireDate;
+        private double _value;
+        public Item(string name, string productionDate, string expireDate, double value)
+        {
+            this._name = name;
+            this._value = value;
+            this._expireDate = expireDate;
+            this._productionDate = productionDate;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public double GetValue()
+        {
+            return _value;
+        }
+        public string GetProdDate()
+        {
+            return _productionDate;
+        }
+        public string GetExpireDate()
+        {
+            return _expireDate;
+        }
+
+        public string GetAllAtributes()
+        {
+            return Convert.ToString(_name + " " + _value + " " + _productionDate + " " + _expireDate);
+        }
+    }
+}
